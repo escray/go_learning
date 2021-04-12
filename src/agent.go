4a768f9bc581565bafe2aa8f06277298b3545e5a
escray <zescray@gmail.com>
2021-04-12 15:48:16 +0800

    micro-kernel framework

--- src/agent.go
+++ src/agent.go
@@ -3,7 +3,9 @@ package go_learning
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
+	"sync"
 )
 
 const(
@@ -37,8 +39,8 @@ type EventReceiver interface {
 type Collector interface {
 	Init(evtReceiver EventReceiver) error
 	Start(agtCtx context.Context) error
-	Stor() error
 	Destroy() error
+	Stop() error
 }
 
 type Agent struct {
@@ -49,4 +51,119 @@ type Agent struct {
 	state int
 }
 
+func (agt *Agent) EventProcessGroutine()  {
+	var evtSeg [10]Event
+	for {
+		for i := 0; i < 10; i++ {
+			select {
+			case evtSeg[i] = <-agt.evtBuf:
+			case <-agt.ctx.Done():
+				return
+			}
+		}
+		fmt.Println(evtSeg)
+	}
+}
 
+func NewAgent(sizeEvtBuf int) *Agent {
+	agt := Agent{
+		collectors: map[string]Collector{},
+		evtBuf: make(chan Event, sizeEvtBuf),
+		state: Waiting,
+	}
+
+	return &agt
+}
+
+func (agt *Agent) RegisterCollector(name string, collector Collector) error {
+	if agt.state != Waiting {
+		return ErrorWrongState
+	}
+	agt.collectors[name] = collector
+	return collector.Init(agt)
+}
+
+func (agt *Agent) startCollectors() error {
+	var err error
+	var errs CollectorsError
+	var mutex sync.Mutex
+
+	for name, collector := range agt.collectors {
+		go func(name string, collector Collector, ctx context.Context) {
+			defer func() {
+				mutex.Unlock()
+			}()
+			err = collector.Start(ctx)
+			mutex.Lock()
+			if err != nil {
+				errs.CollectorErrors = append(errs.CollectorErrors,
+					errors.New(name+":"+err.Error()))
+			}
+		}(name, collector, agt.ctx)
+	}
+	if len(errs.CollectorErrors) == 0 {
+		return nil
+	}
+	return errs
+}
+
+func (agt *Agent) stopCollectors() error {
+	var err error
+	var errs CollectorsError
+	for name, collector := range agt.collectors {
+		if err = collector.Stop(); err != nil {
+			errs.CollectorErrors = append(errs.CollectorErrors,
+				errors.New(name+":"+err.Error()))
+		}
+	}
+	if len(errs.CollectorErrors) == 0 {
+		return nil
+	}
+
+	return errs
+}
+
+func (agt *Agent) destroyCollectors() error {
+	var err error
+	var errs CollectorsError
+	for name,collector := range agt.collectors {
+		if err = collector.Destroy(); err != nil {
+			errs.CollectorErrors = append(errs.CollectorErrors,
+				errors.New(name+""+err.Error()))
+		}
+	}
+	if len(errs.CollectorErrors) == 0 {
+		return nil
+	}
+	return errs
+}
+
+func (agt *Agent) Start() error {
+	if agt.state != Waiting {
+		return ErrorWrongState
+	}
+	agt.state = Running
+	agt.ctx, agt.cancel = context.WithCancel(context.Background())
+	go agt.EventProcessGroutine()
+	return agt.startCollectors()
+}
+
+func (agt *Agent) Stop() error {
+	if agt.state != Running {
+		return ErrorWrongState
+	}
+	agt.state = Waiting
+	agt.cancel()
+	return agt.stopCollectors()
+}
+
+func (agt *Agent) Destroy() error {
+	if agt.state != Waiting {
+		return ErrorWrongState
+	}
+	return agt.destroyCollectors()
+}
+
+func (agt *Agent) OnEvent(evt Event)  {
+	agt.evtBuf <- evt
+}
